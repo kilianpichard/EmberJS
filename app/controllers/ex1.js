566af18715a37dd06ba0d37b5e2a1ab283efc3b7
Kilian Pichard <kil.pichard@icloud.com>
2021-01-25 10:43:12 +0100

    add tracked var and clear

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class Ex1Controller extends Controller {
+  @tracked count = 0;
+  @tracked content = 'Entrez votre texte';
+  @tracked info = '';
+
+  @action
+  clear() {
+    this.content = '';
+  }
 }
